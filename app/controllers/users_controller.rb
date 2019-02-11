c83be3c332f16fc910a5aa9699c6b96496b66653
Laura <laurao148@gmail.com>
2019-02-11 21:46:52 +0000

    Automatically backed up by Learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,9 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+<<<<<<< HEAD
 
+=======
+  
+>>>>>>> 5c3bcabc900aff59ac4bfd28d04c36d12d10b03b
 end
